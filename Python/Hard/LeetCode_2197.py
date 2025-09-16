da0e194ae2adfd6eb62c61a2327b0a17ff8bf365
Mahesh-Sarkar06 <maheshsarkar97.ms@gmail.com>
2025-09-16 23:11:50 +0530

    Added daily challenge Replace Non-Coprime numbers in array

---
+++ Python/Hard/LeetCode_2197.py
@@ -0,0 +1,45 @@
+# You are given an array of integers nums. Perform the following steps:
+
+# Find any two adjacent numbers in nums that are non-coprime.
+# If no such numbers are found, stop the process.
+# Otherwise, delete the two numbers and replace them with their LCM (Least Common Multiple).
+# Repeat this process as long as you keep finding two adjacent non-coprime numbers.
+# Return the final modified array. It can be shown that replacing adjacent non-coprime numbers in any arbitrary order will lead to the same result.
+
+# The test cases are generated such that the values in the final array are less than or equal to 108.
+
+# Two values x and y are non-coprime if GCD(x, y) > 1 where GCD(x, y) is the Greatest Common Divisor of x and y.
+
+
+from math import gcd
+from typing import List
+
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        # Defining an empty stack to store adjacent pairs
+        stack = []
+
+        # Iterate through each nums and append to stack list
+        for num in nums:
+            stack.append(num)
+
+            # Calculate till number of elements in stack is more than 1
+            while len(stack) > 1:
+                # Since list is traversed from left to right and we want latest adjacent pairs,
+                # which resides in right most positions
+                n1, n2 = stack[-2], stack[-1]
+                # Calculate gcd
+                res = gcd(n1, n2)
+
+                # Check if it is co-prime or not. If Yes -> break the loop
+                if res == 1:
+                    break
+
+                # Pop out the right most elements
+                stack.pop()
+                stack.pop()
+                # Calculate LCM and append to the stack
+                stack.append(n1 * n2 // res)
+                
+
+        return stack
