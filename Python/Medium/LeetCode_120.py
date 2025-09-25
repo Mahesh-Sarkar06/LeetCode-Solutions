bdc76bdbc564f26ae99682075d1c4ce5aa29cd4a
Mahesh-Sarkar06 <maheshsarkar97.ms@gmail.com>
2025-09-25 23:29:28 +0530

    Added daily challenge Triangle

---
+++ Python/Medium/LeetCode_120.py
@@ -0,0 +1,16 @@
+# Given a triangle array, return the minimum path sum from top to bottom.
+
+# For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+
+from typing import List
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle[-1][:]
+
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1])
+
+        return dp[0]
